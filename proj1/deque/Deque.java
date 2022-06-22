355d0739d791131fd9ab9ed14b8ba7a46021cc85
Guang-Hou <gfeng.hou@gmail.com>
2022-06-21 22:15:01 -0400

    Deque checkstyle fix

--- proj1/deque/Deque.java
+++ proj1/deque/Deque.java
@@ -6,40 +6,40 @@ public interface Deque<T> {
     /**
      * Adds an item of type T to the front of the deque. You can assume that item is never null.
      */
-    public void addFirst(T item);
+    void addFirst(T item);
 
     /**
      * Adds an item of type T to the back of the deque. You can assume that item is never null.
      */
-    public void addLast(T item);
+    void addLast(T item);
 
     /**
      * Returns true if deque is empty, false otherwise.
      */
-    public default boolean isEmpty() {
+    default boolean isEmpty() {
         return size() == 0;
     };
 
     /**
      * Returns the number of items in the deque.
      */
-    public int size();
+    int size();
 
     /**
      * Prints the items in the deque from first to last, separated by a space.
      * Once all the items have been printed, print out a new line.
      */
-    public void printDeque();
+    void printDeque();
 
     /**
      * Removes and returns the item at the front of the deque. If no such item exists, returns null.
      */
-    public T removeFirst();
+    T removeFirst();
 
     /**
      * Removes and returns the item at the back of the deque. If no such item exists, returns null.
      */
-    public T removeLast();
+    T removeLast();
 
     /**
      * Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
@@ -47,13 +47,14 @@ public interface Deque<T> {
      * @param index
      * @return item at index
      */
-    public T get(int index);
+    T get(int index);
 
     /**
-     * The Deque objects we’ll make are iterable (i.e. Iterable<T>) so we must provide this method to return an iterator.
+     * The Deque objects we’ll make are iterable (i.e. Iterable<T>)
+     * so we must provide this method to return an iterator.
      * @return
      */
-    public Iterator<T> iterator();
+    Iterator<T> iterator();
 
     /**
      * Returns whether or not the parameter o is equal to the Deque.
@@ -63,6 +64,6 @@ public interface Deque<T> {
      * @param o
      * @return true or false
      */
-    public boolean equals(Object o);
+    boolean equals(Object o);
 
 }
